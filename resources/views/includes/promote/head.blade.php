4f4fab3b25aaca468e27db9b73f07d8383582ea0
Bantita Chuatrakul <zherspq9r@gmail.com>
2022-08-03 12:44:35 +0700

    update 03/08/2022

---
+++ resources/views/includes/promote/head.blade.php
@@ -0,0 +1,27 @@
+<link href="{{ asset('template/promote/assets/img/favicon.png') }}  " rel="icon">
+  <link href="{{ asset('template/promote/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('template/promote/assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('template/promote/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('template/promote/assets/css/style.css') }}" rel="stylesheet">
+
+<script src="{{ asset('template/promote/assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('template/promote/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('template/promote/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('template/promote/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('template/promote/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('template/promote/assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('template/promote/assets/js/main.js') }}"></script>
---
+++ resources/views/includes/promote/header.blade.php
@@ -0,0 +1,21 @@
+<header id="header" class="fixed-top d-flex align-items-cente">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.php">Hint Coffee Roaster</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="house-blend.php">House Blend</a></li>
+          <li><a class="nav-link scrollto" href="single-origin.php">Single Orgin</a></li>
+          <li><a class="nav-link scrollto" href="beverage.php">Beverage Menu</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+      <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
+
+    </div>
+  </header>
